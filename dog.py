1d06b406f3993943b5809588d2c401c8a7d1e498
ARIF <arif@kali.arif>
2020-11-24 18:31:04 -0500

    Making an instance from Dog class

--- dog.py
+++ dog.py
@@ -1,11 +1,17 @@
 class Dog():
     def __init__(self, name, age):
-        self.name = name.
+        self.name = name
         self.age = age
+    
     def sit(self):
         print(f"{self.name.title()} is now sitting.")
 
     def roll_over(self):
-        print(f"{self.name.title()} rolled ober.")
+        print(f"{self.name.title()} rolled over!!!")
 
-        
+my_dog = Dog('willi', 3)
+
+print(f"My dog name is {my_dog.name.title()}.")
+print(f"My dog is {str(my_dog.age)} years old.")
+my_dog.sit()
+my_dog.roll_over()
