d8c59d3df395d453d0b07ed4a83082b43d4cc912
ARIF <arif@kali.arif>
2020-11-25 23:21:20 -0500

    Added a file named Ex9.3.py and maked a class called User().

---
+++ Ex9.3.py
@@ -0,0 +1,24 @@
+class User():
+    def __init__(self, first_name, last_name, *others_info):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.others = others_info
+
+    def describe_user(self):
+        fullName = self.first_name + self.last_name
+        print('Name and Address:')
+        print(fullName.title())
+        for other in self.others:
+            if other == self.others[-1]:
+                print(other.title())
+                continue
+            print(other.title(), end=', ')
+    
+    def greed_user(self):
+        fullName = self.first_name + ' ' + self.last_name
+        print(f'Hello {fullName.title()}!!!')
+
+user = User('md.', 'uzzal', 'pakshia', 'kalukhali', 'rajbari')
+user.describe_user()
+user.greed_user()
+        
