1cf385ce87d381a620813a5210108d9164c19663
arifcse4 <arif.cse4@gmail.com>
2020-11-27 00:30:31 -0500

    This is exersice 9.5. Here a user loging page. Here if you enter your correct password it show your details otherwise not. You can enter your password three times. After that need to resat password. For that task you have to enter your last name and district.

--- Ex9.3.py
+++ Ex9.3.py
@@ -3,21 +3,59 @@ class User():
         self.first_name = first_name
         self.last_name = last_name
         self.others = others_info
+        self.password1 = others_info[-1]
+        self.loging_attempt = 0
 
     def describe_user(self):
+
         fullName = self.first_name + self.last_name
+
         print('Name and Address:')
         print(fullName.title())
+
         for other in self.others:
-            if other == self.others[-1]:
+            if other == self.others[-2]:
                 print(other.title())
-                continue
+                break
             print(other.title(), end=', ')
+        
+        print(f"Value of loging attempt: {str(self.loging_attempt)}")
+        self.reset_loging_attempt()
     
     def greed_user(self):
         fullName = self.first_name + ' ' + self.last_name
         print(f'Hello {fullName.title()}!!!')
 
-user = User('md.', 'uzzal', 'pakshia', 'kalukhali', 'rajbari')
-user.describe_user()
-user.greed_user()
+    def increment_login_attempts(self):
+        self.loging_attempt += 1
+
+    def loging(self):
+        password2 = input("Enter your password: ")
+        
+        if password2 == self.password1:
+            self.increment_login_attempts()
+            self.describe_user()
+            self.greed_user()
+        elif self.loging_attempt < 3:
+            print("Enter correct password!!!")
+            self.increment_login_attempts()
+            self.loging()
+        else:
+            self.reset_password()
+    
+    def reset_password(self):
+        ln = input("Enter your last name: ")
+        dis = input("Enter your district name: ")
+
+        if ln == self.last_name and dis == self.others[-2]:
+            self.password1 = input("Enter new password: ")
+            print("Your password has been reseted. Now enter your password.")
+            self.reset_loging_attempt()
+            self.loging()
+
+    def reset_loging_attempt(self):
+        self.loging_attempt = 0
+        print(f"Reset loging attempt to {str(self.loging_attempt)}")
+
+user = User('md.', 'uzzal', 'pakshia', 'kalukhali', 'rajbari', 'uzzal@1234')
+user.loging()
